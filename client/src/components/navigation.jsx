bc63a59aaebd418d9128c0965060b1b51be6ddbc
Charlie <charlieboyle252@gmail.com>
2025-03-26 00:46:09 -0500

    Added more css for sign-up form, creation of css for navigation component and basic starter css for main web page route buttons

--- client/src/components/navigation.jsx
+++ client/src/components/navigation.jsx
@@ -1,3 +1,4 @@
+import "./navigation.css"
 import React from "react";
 import { Link, Outlet } from 'react-router-dom';
 
--- client/src/components/signup.jsx
+++ client/src/components/signup.jsx
@@ -1,65 +1,76 @@
 import React from "react";
-import "./components.css";
+import "./signup.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faYoutube } from '@fortawesome/free-brands-svg-icons';
+import { faFacebook } from '@fortawesome/free-brands-svg-icons';
+import { faBandcamp } from '@fortawesome/free-brands-svg-icons';
+import { faSpotify } from '@fortawesome/free-brands-svg-icons';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faTiktok } from '@fortawesome/free-brands-svg-icons';
+
 
 function Signup() {
     return (
         <>
-            <h1>Sign Up</h1>
-
             <div id="sign-up-container">
+                <h1 id="sign-up">Sign Up</h1>
+                {/* form starts */}
                 <form id="sign-up-form">
                     <div id="general-info">
                         <h3 id="who-are-you">Who are you?</h3>
-                        <label>Artist Name:
-                            <input></input>
+                        <label id="artist-name">Artist Name:
+                            <input classname="top-input"></input>
                         </label>
 
-                        <label>Years Together:
-                            <input></input>
+                        <label id="years-together">Years Together:
+                            <input classname="top-input"></input>
                         </label>
 
-                        <label>Music Genre:
-                            <input></input>
-                        </label>
-
-                        <label>About Us:
-                            <input></input>
-                        </label>
-
-                        <label>Images:
-                            <input></input>
+                        <label id="music-genre">Music Genre:
+                            <input classname="top-input"></input>
                         </label>
                     </div>
+
+
+                    <div id="images-section">
+                        <h3 id="images-header">Images</h3>
+                    </div>
+
 
                     <div id="links">
                         <h3 id="links-heading">Music and Social Media</h3>
 
-                        <label id="spotify-label"> Spotify Link:
+                        <label id="spotify-label"> <FontAwesomeIcon icon={faSpotify} size="lg" style={{ color: "#1edc70", }} />
                             <input></input>
                         </label>
 
-                        <label id="instagram-label"> Instagram Link:
+                        <label id="instagram-label"> <FontAwesomeIcon icon={faInstagram} size="lg" style={{ color: "#ff0088", }} />
                             <input></input>
                         </label>
 
-                        <label id="tiktok-label"> TikTok Link:
+                        <label id="tiktok-label"> <FontAwesomeIcon icon={faTiktok} size="lg" style={{ color: "#000000", }} />
                             <input></input>
                         </label>
 
-                        <label id="youtube-label"> Youtube Link:
+                        <label id="youtube-label"> <FontAwesomeIcon icon={faYoutube} size="lg" style={{ color: "#ff0000", }} />
                             <input></input>
                         </label>
 
-                        <label id="facebook-label"> Facebook Link:
+                        <label id="facebook-label"> <FontAwesomeIcon icon={faFacebook} size="lg" style={{ color: "#2671f2", }} />
                             <input></input>
                         </label>
 
-                        <label id="bandcamp-label"> Bandcamp Link:
+                        <label id="bandcamp-label"> <FontAwesomeIcon icon={faBandcamp} size="lg" style={{ color: "#02aad4", }} />
                             <input></input>
                         </label>
                     </div>
 
-                    <button>Submit</button>
+                    <div>
+                        <h3 id="tell-us-more">Tell us more</h3>
+                        <input id="about-us-input" placeholder="Tell others about who you are and the music you create!"></input>
+                    </div>
+
+                    <button id="form-submit">Submit</button>
                 </form>
             </div>
         </>
